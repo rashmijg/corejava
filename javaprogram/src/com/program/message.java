948f670883e71f267a8aa40378626963d7ee32c0
rashmi <rashmiskp222@gmail.com>
2019-10-01 13:53:10 +0530

    completed

--- javaprogram/src/com/program/message.java
+++ javaprogram/src/com/program/message.java
@@ -5,7 +5,8 @@ public class message {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
+		System.out.println("helloworld");
+
 
 	}
 
